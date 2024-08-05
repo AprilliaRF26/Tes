c35c773d446f50019ad582405dd95c8114fafd0a
Aprillia Rafifah Fitriani <aprillia_fitriani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:11:33 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+   public static void main(String[] args) {    
+   String nama="Lia"; 
+   String kelas="X RPL 5";   
+   System.out.println("Halo, nama saya" + nama + "dari kelas "+kelas);    
+    }
+}
